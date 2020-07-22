80c05fc5a76ba79204d56c3a2bc6a4fcfb09ba10
unknown <Alexander Miles>
2020-07-22 10:07:17 +0100

    You can play with the brightness!

--- Lamp/Program.cs
+++ Lamp/Program.cs
@@ -12,40 +12,35 @@ namespace Lamp
     {
         public static void Main(string[] args)
         {
-            //Let's test a coloured bulb
-            //IColouredBulb bulb = new ColouredBulb("192.168.1.139");
-            //Normal bulb tbh
             IBulb lamp = new Bulb("192.168.1.164");
 
-            //Let's make sure we're connected
             Console.WriteLine($"Is the lamp on? {lamp.isNetworked()} connected at {lamp.GetNetworkAddress()}");
 
-            //Running the operation and getting the values from these
             var systemInfo = lamp.GetBulbInfo();
-            //var rebootResult = lamp.Reboot(5);
-            //var aliasResult = lamp.SetAlias("New name");
-
-            //var bulbColour = lamp.GetColour();
-
-
-            //Get the current Hue
-            //Console.WriteLine(bulbColour.Hue);
-
-            //Set the bulb to green
-            //var result = lamp.SetColour(new LightState(1, 5, "normal", 1, 120, 100, 0, 100));
-            var result = lamp.SetBrightness(10);
+            var result = lamp.SetBrightness(100);
+            
             Console.WriteLine(result);
-            //Get the colour of the bulb
-            //var newBulbColour = bulb.Get  Colour();
-
-            //Console.WriteLine(newBulbColour.Hue);
-            //bulb.TogglePower();
-            //lamp.TogglePower();
-            //Let's print out to the console
-            //Console.WriteLine(result);
-            //Console.WriteLine(rebootResult.ErrorCode);
             Console.WriteLine(systemInfo.IsDimmable);
             Console.WriteLine(systemInfo.Model);
+
+            bool keepLooping = true;
+            do
+            {
+                Console.WriteLine("Please input either a brightness value (int) or (E)xit");
+                string input = Console.ReadLine();
+                switch(input) {
+                    case "E":
+                    case "e":
+                    case "exit":
+                    case "Exit":
+                        keepLooping = false;
+                        break;
+                    default:
+                        var localResult = lamp.SetBrightness(Convert.ToInt32(input));
+                        Console.WriteLine(localResult);
+                        break;
+                }
+            } while (keepLooping);
             Console.ReadLine();
         }
     }
